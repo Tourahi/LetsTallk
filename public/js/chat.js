d6057d2c6469d06be77b1273769cb9640b3eb67c
maromaroXD <amine-fb-fb@hotmail.fr>
2020-11-09 21:05:54 +0100

    Styling the chat room.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,9 @@
 const socket = io();
 
-//Elements
-const $form = document.querySelector('#sendMsg');
-const $messages = document.querySelector('#messages');
 
-
-//Templates
-const messageTemplate = document.querySelector('#message-template').innerHTML;
-
-socket.on('message',(msg) => {
-  console.log(msg);
-  const html = Mustache.render(messageTemplate, {
-    message : msg.txt,
-    createdAt : moment(msg.createdAt).format('h:mm a')
-  });
-  $messages.insertAdjacentHTML('beforeend',html);
-});
-
-$form.addEventListener('submit',(e) => {
-  e.preventDefault();
-  socket.emit('fromMe', e.target.elements.txt.value);
-  e.target.elements.txt.value = '';
-  e.target.elements.txt.focus();
-});
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
